73e08c033f86f01e309dd6e2e15938f9b34c45b0
Samarpreet Singh <bhamrasamar02@gmail.com>
2025-07-17 20:31:58 +0530

    Update: latest changes to resume and styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import "../styles/Landing.css";
+import "../styles/landing.css";
 import resumelogo from "../assets/Screenshot 2025-07-12 165421.png";
 
 export default function Landing() {
